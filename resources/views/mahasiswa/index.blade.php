783cbe3a5b2034d26cb190c5bdadb88a4bf9bac4
setyawan1234 <twentyzee1@gmail.com>
2022-03-29 18:48:52 +0700

    update index.blade.php

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -23,6 +23,8 @@
             <th>Kelas</th>
             <th>Jurusan</th>
             <th>No_Handphone</th>
+            <th>Email</th>
+            <th>Tanggal Lahir</th>
             <th width="280px">Action</th>
         </tr>
         @foreach ($mahasiswas as $Mahasiswa)
@@ -33,6 +35,8 @@
                 <td>{{ $Mahasiswa->Kelas }}</td>
                 <td>{{ $Mahasiswa->Jurusan }}</td>
                 <td>{{ $Mahasiswa->No_Handphone }}</td>
+                <td>{{ $Mahasiswa->Email }}</td>
+                <td>{{ $Mahasiswa->TanggalLahir }}</td>
                 <td>
                     <form action="{{ route('mahasiswa.destroy', $Mahasiswa->Nim) }}" method="POST">
 
