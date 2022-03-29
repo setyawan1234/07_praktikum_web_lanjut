32d3bbb3ee341f223385234778deed1bf3f7f657
setyawan1234 <twentyzee1@gmail.com>
2022-03-29 18:49:23 +0700

    tambah nilai $filliable email,ttl

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -16,6 +16,8 @@ class Mahasiswa extends Model
     protected $fillable = [
         'Nim',
         'Nama',
+        'Email',
+        'TanggalLahir',
         'Kelas',
         'Jurusan',
         'No_Handphone',
