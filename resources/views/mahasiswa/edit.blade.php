c6b68fda99a44bf3d049776e64b31c210a3e1ddc
setyawan1234 <twentyzee1@gmail.com>
2022-03-29 18:48:44 +0700

    update edit.blade.php

--- resources/views/mahasiswa/edit.blade.php
+++ resources/views/mahasiswa/edit.blade.php
@@ -45,6 +45,24 @@
                             <input type="No_Handphone" name="No_Handphone" class="form-control" id="No_Handphone"
                                 value="{{ $Mahasiswa->No_Handphone }}" aria- describedby="No_Handphone">
                         </div>
+
+                        <div class="form-group">
+                            <label for="Email">Email</label>
+
+                            <input type="Email" name="Email" class="form-control" id="Email"
+                                value="{{ $Mahasiswa->Email }}" aria- describedby="Email">
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="TanggalLahir">Tanggal Lahir</label>
+
+                            <input type="TanggalLahir" name="TanggalLahir" class="form-control" id="TanggalLahir"
+                                value="{{ $Mahasiswa->TanggalLahir }}" aria- describedby="TanggalLahir">
+                        </div>
+
+
+
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
